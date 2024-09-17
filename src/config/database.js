db9aa21188e78934dfb4babaa1373c72086b9904
Santiago <srheredia26@gmail.com>
2024-09-17 18:50:51 -0500

    Correction references to Mongo

--- src/config/database.js
+++ src/config/database.js
@@ -4,15 +4,15 @@ dotenv.config();
 
 const Uri = process.env.MONGO_URI;
 
-//Connect to mongo
+// Connect to MongoDB
 const connectMongoDB = async () => {
     try {
         await mongoose.connect(Uri);
         console.log('Connected to MongoDB');
     } catch (e) {
-        console.error('Conection failed: ', error.message);
+        console.error('Connection failed: ', e.message);
         process.exit(1);
     }
-}
+};
 
 export { connectMongoDB };

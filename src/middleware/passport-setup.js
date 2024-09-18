09b92dd095f75a25ab5f1cb3363ad14a67545a7a
Santiago <srheredia26@gmail.com>
2024-09-17 22:46:07 -0500

    Change passport urls

--- src/middleware/passport-setup.js
+++ src/middleware/passport-setup.js
@@ -6,7 +6,7 @@ import User from '../models/user.model.js';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'https://geosecurity.onrender.com/auth/google/callback'
 
 }, async (accessToken, refreshToken, profile, done) => {
     try {

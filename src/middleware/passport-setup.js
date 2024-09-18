1e7bb707c31366cd7bee5fb6f7ca314bb150bbda
Santiago <srheredia26@gmail.com>
2024-09-17 23:12:56 -0500

    Change Google Credentials

--- src/middleware/passport-setup.js
+++ src/middleware/passport-setup.js
@@ -1,13 +1,19 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/user.model.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const callbackURL = process.env.NODE_ENV === 'production'
+  ? process.env.PROD_GOOGLE_CALLBACK_URL // URL de producción
+  : process.env.GOOGLE_CALLBACK_URL; // URL local de desarrollo
 
 // Configuración de la estrategia de Google
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://geosecurity.onrender.com/auth/google/callback'
-
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: callbackURL
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // Verificar si el usuario ya existe en la base de datos
